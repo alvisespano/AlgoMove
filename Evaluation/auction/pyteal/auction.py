ee417a5434cfc83029e60ba2811b81f88ded66e1
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2026-01-20 21:16:41 +0100

    PyTEAL auction update

--- Evaluation/auction/pyteal/auction.py
+++ Evaluation/auction/pyteal/auction.py
@@ -2,83 +2,45 @@ import pyteal as pt
 from pyteal import Seq, Assert, Txn, Global, TxnField, TxnType, InnerTxnBuilder, Int
 import beaker
 
-class States:
-    WAIT_START = Int(0)
-    WAIT_CLOSING = Int(1)
-    CLOSED = Int(2)
+class Expired:
+    FALSE = Int(1)
+    TRUE = Int(2)
+
 
 class AuctionState:
-    obj = beaker.GlobalStateValue(
+    auctioneer = beaker.LocalStateValue(
         stack_type=pt.TealType.bytes,
         static=True,
-        descr="Descriptor of the auctioned object",
+        descr="auctioneer of the object",
     )
-    seller = beaker.GlobalStateValue(
-        stack_type=pt.TealType.bytes,
-        static=True,
-        descr="Seller of the object",
-    )
-    end_time = beaker.GlobalStateValue(
+    expired = beaker.LocalStateValue(
         stack_type=pt.TealType.uint64,
-        static=True,
-        descr="The last round of the bidding window",
+        descr="Flag indicating if the auction has expired",
     )
-    highest_bid = beaker.GlobalStateValue(
+    highest_bid = beaker.LocalStateValue(
         stack_type=pt.TealType.uint64,
         descr="The current winning bid",
     )
-    highest_bidder = beaker.GlobalStateValue(
+    highest_bidder = beaker.LocalStateValue(
         stack_type=pt.TealType.bytes,
         descr="The current winning bidder",
-    )
-    state = beaker.GlobalStateValue(
-        stack_type=pt.TealType.uint64,
-        descr="The current logical state: WAIT_START/WAIT_CLOSING/CLOSED",
     )
     bid = beaker.LocalStateValue(
         stack_type=pt.TealType.uint64,
         descr="Bid of an actor",
     )
 
-app = beaker.Application("Auction", state=AuctionState())
-
-@app.create
-def create(
-    obj_: pt.abi.String,
-    starting_bid_: pt.abi.Uint64,
-):
-    return Seq(
-        # If not present we would have multiple problems:
-        # - the first bidder would not be able to bid, as the MBR of the contract would not be satisfied
-        # - when reclaiming the bidded funds, bidder could be stuck not being able to reclaim their bid 
-        #   amount, as that would make the contract balance go below the MBR
-        Assert(starting_bid_.get() >= Int(100000),
-               comment="Must provide at least MBR"),
-
-        # Initialize contract state
-        app.state.obj.set(obj_.get()),
-        app.state.seller.set(Txn.sender()),
-        app.state.highest_bid.set(starting_bid_.get()),
-
-        # Transition into WAIT_START
-        app.state.state.set(States.WAIT_START),
-    )
+app = beaker.Application("Auction2", state=AuctionState())
 
 @app.external
 def start(
-    duration_: pt.abi.Uint64,
+    starting_bid_: pt.abi.PaymentTransaction,
 ):
     return Seq(
-        Assert(app.state.state == States.WAIT_START,
-               comment="Action must be waiting to be started"),
-        Assert(Txn.sender() == app.state.seller,
-               comment="Callable only by the seller"),
-
+        app.state.auctioneer[Txn.sender()].set(Txn.sender()),
+        app.state.highest_bid[Txn.sender()].set(starting_bid_.get().amount()),
         # Set auction end time
-        app.state.end_time.set(Global.round() + duration_.get()),
-
-        # Transition into WAIT_CLOSING
-        app.state.state.set(States.WAIT_CLOSING),
+        app.state.expired[Txn.sender()].set(Expired.FALSE),
     )
 
 @app.external(method_config=pt.MethodConfig(
@@ -86,55 +48,38 @@ def start(
     no_op=pt.CallConfig.CALL,
 ))
 def bid(
+    auctioneer: pt.abi.Address,
     deposit: pt.abi.PaymentTransaction,
+    
 ):
     return Seq(
-        Assert(app.state.state == States.WAIT_CLOSING,
-               comment="Auction must be running"),
-        Assert(Global.round() < app.state.end_time,
-               comment="Bids can only happen before the end of the bid period"),
-        Assert(deposit.get().amount() > app.state.highest_bid,
+        Assert(app.state.expired[auctioneer.get()] == Expired.FALSE,
+               comment="Bids can only be placed before expiration"),
+        Assert(deposit.get().amount() > app.state.highest_bid[auctioneer.get()],
                comment="Bid must be higher than previous bid"),
 
-        # Send back to the bidder their previous bid (0 at first bid)
-        pay_or_close(Txn.sender(), app.state.bid[Txn.sender()]),
+        # Send back to the highest bidder their previous bid (0 at first bid)
+        pay_or_close(app.state.highest_bidder[auctioneer.get()].get(), app.state.bid[app.state.highest_bidder[auctioneer.get()].get()]),
 
         # Update bid and highest bid info
         app.state.bid[Txn.sender()].set(deposit.get().amount()),
-        app.state.highest_bidder.set(Txn.sender()),
-        app.state.highest_bid.set(deposit.get().amount()),
-    )
-
-@app.external
-def withdraw():
-    return Seq(
-        Assert(app.state.state != States.WAIT_START,
-               comment="Auction must be running"),
-        Assert(Txn.sender() != app.state.highest_bidder,
-               comment="The highest bidder cannot withdraw their bid"),
-
-        # Send bid back to caller
-        pay_or_close(Txn.sender(), app.state.bid[Txn.sender()]),
-
-        # Reset bid amount
-        app.state.bid[Txn.sender()].set(Int(0)),
+        app.state.highest_bidder[Txn.sender()].set(Txn.sender()),
+        app.state.highest_bid[Txn.sender()].set(deposit.get().amount()),
     )
 
 @app.external
 def end():
     return Seq(
-        Assert(Txn.sender() == app.state.seller,
-               comment="Only the seller can close the contract"),
-        Assert(app.state.state == States.WAIT_CLOSING,
+        Assert(Txn.sender() == app.state.auctioneer[Txn.sender()],
+               comment="Only the auctioneer can close the contract"),
+        Assert(app.state.expired[Txn.sender()] == Expired.FALSE,
                comment="Auction must be running"),
-        Assert(Global.round() >= app.state.end_time,
-               comment="Bid period must be over"),
 
-        # Pay seller the highest bid
-        pay_or_close(app.state.seller, app.state.highest_bid),
+        # Pay auctioneer the highest bid
+        pay_or_close(app.state.auctioneer[Txn.sender()], app.state.highest_bid[Txn.sender()]),
 
         # Transition to end state
-        app.state.state.set(States.CLOSED),
+        app.state.expired[Txn.sender()].set(Expired.TRUE),
     )
 
 @pt.Subroutine(pt.TealType.none)
